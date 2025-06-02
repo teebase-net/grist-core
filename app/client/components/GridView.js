6b9905a8e6a423e141d61b99521a4c4d703ec247
davidmh72 <david.m.hawley@gmail.com>
2025-06-02 16:38:30 +1000

    Update GridView.js

--- app/client/components/GridView.js
+++ app/client/components/GridView.js
@@ -1,4 +1,4 @@
-/**
+30/**
  * GridView.js
  *
  * Implements the main grid view component in Grist, responsible for rendering table data,
@@ -84,7 +84,7 @@ const SHORT_CLICK_IN_MS = 500;
 const PLUS_WIDTH = 40;
 // size of the row number field (we assume 4rem, 1rem = 13px in grist)
 
-const ROW_NUMBER_WIDTH = 52;    // DMH MOD was 52px
+const ROW_NUMBER_WIDTH = 30;    // DMH MOD was 52px
 
 
 /**

6872a684c57c847140d627f0132210ce7a7971a1
davidmh72 <david.m.hawley@gmail.com>
2025-06-25 17:46:18 +1000

    Update TabBarView.ts

--- app/client/components/TabBarView.ts
+++ app/client/components/TabBarView.ts
@@ -1,4 +1,4 @@
-import { dom, computed } from 'grainjs';
+import { dom, styled, computed } from 'grainjs';
 import { ViewSectionRec } from 'app/client/models/DocModel';
 import { GristDoc } from 'app/client/components/GristDoc';
 
@@ -15,7 +15,7 @@ const TabBarView = (gristDoc: GristDoc) => {
           tab(
             { 
               style: { gridRow: i + 1 },
-              onClick: () => console.log(`Selected tab ${sec.label || i}`),
+              onClick: () => console.log(`Selected tab ${sec.widgetType || i}`), // Use widgetType
               draggable: true,
               onDragStart: (e: DragEvent) => e.dataTransfer?.setData('text/plain', String(i)),
               onDragOver: (e: DragEvent) => e.preventDefault(),
@@ -26,7 +26,7 @@ const TabBarView = (gristDoc: GristDoc) => {
                 if (fromIndex !== toIndex) reorderTabs(fromIndex, toIndex);
               }
             },
-            sec.label || sec.widgetType || `Tab ${i + 1}`
+            sec.widgetType || `Tab ${i + 1}` // Use widgetType
           )
         )))
       )
@@ -37,7 +37,7 @@ const TabBarView = (gristDoc: GristDoc) => {
     const newSections = [...sections.get()];
     const [moved] = newSections.splice(fromIndex, 1);
     newSections.splice(toIndex, 0, moved);
-    console.log('Reordered tabs:', newSections.map(s => s.label || s.widgetType));
+    console.log('Reordered tabs:', newSections.map(s => s.widgetType || `Tab ${newSections.indexOf(s) + 1}`));
   }
 };
 
